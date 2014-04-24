d1e23d4f15a1a424eced31532575e5f674bfaaf2
Weerasak Chongnguluam <singpor@gmail.com>
2014-04-24 22:15:47 +0700

    Create function GetTimeout

--- main.go
+++ main.go
@@ -4,13 +4,14 @@ import (
     "fmt"
     "net/http"
     "time"
+    "errors"
 )
 
-func main() {
+func GetTimeout(url string, timeout time.Duration) (*http.Response, error) {
     responseChannel := make (chan *http.Response, 1)
     errorChannel := make (chan error, 1)
     go func() {
-        resp, err := http.Get("google.com/")
+        resp, err := http.Get(url)
         if err != nil {
             errorChannel <- err
         } else {
@@ -20,10 +21,19 @@ func main() {
 
     select {
         case resp := <-responseChannel:
-            fmt.Println(resp)
+            return resp, nil
         case err := <-errorChannel:
-            fmt.Println(err)
-        case <-time.After(1*time.Second):
-            fmt.Println("Timeout")
+            return nil, err
+        case <-time.After(timeout):
+            return nil, errors.New("Timeout")
+    }
+}
+
+func main() {
+    resp, err := GetTimeout("https://dl.dropboxusercontent.com/s/t15wh4iw49ekawi/Legend.mp3?dl=1&token_hash=AAF6niX0nqa1GA8d2YaeTpvjtyRl7M1J1TN3goHmAihC3Q", 1*time.Second)
+    if err != nil {
+        fmt.Println(err)
+    } else {
+        fmt.Println(resp)
     }
 }
